c11e338f91b335f6573b16550a2b4ea9eaef2184
Daniel Chiarelli <daniel@Daniels-MacBook-Pro.local>
2023-11-30 21:17:18 -0300

    add modo punitivo

--- app/src/main/java/com/example/a5ex5/MainActivity.kt
+++ app/src/main/java/com/example/a5ex5/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import kotlin.random.Random
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 
 
 class MainActivity : AppCompatActivity() {
@@ -48,20 +49,63 @@ class MainActivity : AppCompatActivity() {
     fun Par(numero: Int){
         if(numero % 2 == 0){
             score++
-            txtScore.text = score.toString()
-            Log.d("score", "par")
-            txtRes.text = Random.nextInt(0,100).toString()
             btnNewGame.visibility = View.VISIBLE
         }
+        else
+        {
+            if(score > 0)
+            {
+                score--
+                btnNewGame.visibility = View.VISIBLE
+            }
+            else if(score <= 0)
+            {
+                mesage()
+            }
+        }
+            txtScore.text = score.toString()
+            Log.d("score", "par")
+            txtRes.text = Random.nextInt(0,10).toString()
+
     }
 
     fun Impar(numero: Int){
         if(numero % 2 != 0){
             score++
-            txtScore.text = score.toString()
-            Log.d("score", "impar")
-            txtRes.text = Random.nextInt(0,100).toString()
             btnNewGame.visibility = View.VISIBLE
         }
+        else
+        {
+            if(score > 0)
+            {
+                score--
+                btnNewGame.visibility = View.VISIBLE
+            }
+            else if(score <= 0)
+            {
+                mesage()
+            }
+        }
+            txtScore.text = score.toString()
+            Log.d("score", "impar")
+            txtRes.text = Random.nextInt(0,10).toString()
     }
+
+    fun mesage(){
+        btnNewGame.visibility = View.INVISIBLE
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Atenção")
+        builder.setMessage("Game Over")
+
+        // Adiciona um botão para fechar o alerta
+        builder.setPositiveButton("OK") { dialog, which ->
+            // Código a ser executado quando o botão OK é pressionado
+        }
+
+        // Cria e exibe o alerta
+        txtRes.text = Random.nextInt(0,10).toString()
+        builder.create().show()
+
+    }
+
 }
